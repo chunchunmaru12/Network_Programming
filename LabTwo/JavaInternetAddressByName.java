2767606af39837e345290a4eaafa57379ba5555a
chunchunmaru12 <princekhan456.pk10@gmail.com>
2025-01-30 07:28:37 +0545

    reverse IP host name

--- LabTwo/JavaInternetAddressByName.java
+++ LabTwo/JavaInternetAddressByName.java
@@ -5,7 +5,7 @@ import java.net.*;
 public class JavaInternetAddressByName {
    public static void PrintIPAddress(String host) {
       try {
-         InetAddress address = InetAddress.getByName("localhost");
+         InetAddress address = InetAddress.getByName(host);
          System.out.println(address);
       } catch (UnknownHostException e) {
          System.out.println("Error: " + e.getMessage());
@@ -13,13 +13,22 @@ public class JavaInternetAddressByName {
 
    }
 
+   public static void PrintHost(String ipAddress) {
+      try {
+         InetAddress address = InetAddress.getByName(ipAddress);
+         System.out.println(address.getHostName());
+      } catch (UnknownHostException e) {
+         System.out.println("Error: " + e.getMessage());
+      }
+   }
+
    public static void main(String[] args) {
       String facebook = "www.facebook.com";
-      String google = "wwww.google.com";
       String localhost = "localhost";
-      PrintIPAddress(facebook);
+      String google = "www.google.com";
       PrintIPAddress(google);
+      PrintIPAddress(facebook);
       PrintIPAddress(localhost);
-
+      PrintHost("163.70.143.35");
    }
 }
