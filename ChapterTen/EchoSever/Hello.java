aca491d8e1f30d259810ba1a80fdd82005395afc
chunchunmaru12 <princekhan456.pk10@gmail.com>
2025-05-07 19:05:52 +0545

    hmm

--- ChapterTen/EchoSever/Hello.java
+++
@@ -1,9 +0,0 @@
-package EchoSever;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-// Remote interface
-public interface Hello extends Remote {
-    void printMsg() throws RemoteException;
-}
